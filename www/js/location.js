92ed62ef7c764719c961009748c8420805993956
jyh947 <jyh947@gmail.com>
2015-01-17 01:47:21 -0500

    Location Saving to Parse

--- www/js/location.js
+++ www/js/location.js
@@ -2,6 +2,11 @@
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
+var PARSE_APP = "3I2hDxytg8b7LF5DcCinCG4capoEk8AV3ZqeUdn5";
+var PARSE_JS = "C6p0uiovPgsGlwjdLbWh04xLwJrln4wmdpilij6n";
+Parse.initialize(PARSE_APP, PARSE_JS);
+LocationObject = Parse.Object.extend("LocationObject");
+
 var onSuccess = function(position) {
     alert('Latitude: '          + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
@@ -11,6 +16,21 @@ var onSuccess = function(position) {
           'Heading: '           + position.coords.heading           + '\n' +
           'Speed: '             + position.coords.speed             + '\n' +
           'Timestamp: '         + position.timestamp                + '\n');
+//Grab the note details, no real validation for now
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var note = new LocationObject();
+    note.save({latitude:latitude, longitude:longitude}, {
+      success:function(object) {
+        console.log("Saved the object!");
+      }, 
+      error:function(object,error) {
+        console.dir(error);
+        alert("Sorry, I couldn't save it.");
+      }
+    });
+
 };
 
 // onError Callback receives a PositionError object
--- www/parse/parse.js
+++ www/parse/parse.js
@@ -5,17 +5,17 @@ var PARSE_JS = "C6p0uiovPgsGlwjdLbWh04xLwJrln4wmdpilij6n";
 $(document).ready(function() {
 	Parse.initialize(PARSE_APP, PARSE_JS);
 
-	NoteObject = Parse.Object.extend("NoteObject");
+	NoteObject = Parse.Object.extend("LocationObject");
 
 	$("#addNoteBtn").on("touchend", function(e) {
 		e.preventDefault();
 		
 		//Grab the note details, no real validation for now
-		var title = $("#noteTitle").val();
-		var body = $("#noteBody").val();
+		var latitude = $("#noteTitle").val();
+		var longitude = $("#noteBody").val();
 
 		var note = new NoteObject();
-		note.save({title:title, body:body}, {
+		note.save({latitude:latitude, longitude:longitude}, {
 			success:function(object) {
 				console.log("Saved the object!");
 			}, 
