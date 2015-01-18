0838025cfbf742b04adc9a12cd6a5b8edf90b887
Blake Bergquist <bbergquist3@gatech.edu>
2015-01-17 20:56:02 -0500

    made it look nice

--- www/js/location.js
+++ www/js/location.js
@@ -9,14 +9,14 @@ Parse.initialize(PARSE_APP, PARSE_JS);
 LocationObject = Parse.Object.extend("LocationObject");
 */
 var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+    // alert('Latitude: '          + position.coords.latitude          + '\n' +
+    //       'Longitude: '         + position.coords.longitude         + '\n' +
+    //       'Altitude: '          + position.coords.altitude          + '\n' +
+    //       'Accuracy: '          + position.coords.accuracy          + '\n' +
+    //       'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+    //       'Heading: '           + position.coords.heading           + '\n' +
+    //       'Speed: '             + position.coords.speed             + '\n' +
+    //       'Timestamp: '         + position.timestamp                + '\n');
 
     //Grab the note details, no real validation for now
     var latitude = position.coords.latitude;
@@ -62,4 +62,4 @@ function onError(error) {
           'message: ' + error.message + '\n');
 }
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+//navigator.geolocation.getCurrentPosition(onSuccess, onError);
